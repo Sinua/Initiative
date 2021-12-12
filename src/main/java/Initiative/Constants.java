445b38f733f461dd345a55330dc8d556ebbf06eb
Mohannad <freeastross@gmail.com>
2021-12-12 20:57:30 +0200

    Change default song

--- src/main/java/Initiative/Constants.java
+++ src/main/java/Initiative/Constants.java
@@ -5,5 +5,5 @@ public class Constants {
     static final String NEWLINE = "\n";
     static final String YOUTUBE_VIDEO_PREIX = "https://www.youtube.com/watch?v=";
     static final char COMMAND_PREFIX = '?';
-    static final String DEFAULT_SONG = "https://www.youtube.com/watch?v=2ZIpFytCSVc";
+    static final String DEFAULT_SONG = "https://www.youtube.com/watch?v=bY_wupeoxVs";
 }
